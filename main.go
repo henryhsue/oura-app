67256d69b006c27d2308605ac3128ecee48797af
Henry Hsue <33713116+henryhsue@users.noreply.github.com>
2020-09-27 13:45:00 -0700

    add struct for sleep summaries

--- main.go
+++ main.go
@@ -31,12 +31,11 @@ func main() {
 
 	// process sleep data
 	body, err = makeRequest(fmt.Sprintf("/v1/sleep?start=%s&end=%s", start, end))
-	fmt.Println(fmt.Sprintf("/v1/sleep?start=%s&end=%s", start, end))
 	check(err)
-	var sleepSummary interface{}
-	err = json.Unmarshal(body, &sleepSummary)
+	var sleepSummaries SleepSummaries
+	err = json.Unmarshal(body, &sleepSummaries)
 	check(err)
-	spew.Dump("Sleep Summary: %+v\n", sleepSummary)
+	spew.Dump("Sleep Summary: %#v\n", sleepSummaries)
 }
 
 func genDatesByDaysOfDuration(days int64) (startStr string, endStr string) {
@@ -56,7 +55,44 @@ type UserInfo struct {
 	Email  string
 }
 
-// TODO: add only necessary fields in sleep summary, with struct
+type SleepSummaries struct {
+	Sleep []SleepSummary // by date
+}
+
+type SleepSummary struct {
+	SummaryDate       string    `json:"summary_date"`
+	PeriodID          int       `json:"period_id"`
+	IsLongest         int       `json:"is_longest"`
+	Timezone          int       `json:"timezone"`
+	BedtimeStart      time.Time `json:"bedtime_start"`
+	BedtimeEnd        time.Time `json:"bedtime_end"`
+	Score             int       `json:"score"`
+	ScoreTotal        int       `json:"score_total"`
+	ScoreDisturbances int       `json:"score_disturbances"`
+	ScoreEfficiency   int       `json:"score_efficiency"`
+	ScoreLatency      int       `json:"score_latency"`
+	ScoreRem          int       `json:"score_rem"`
+	ScoreDeep         int       `json:"score_deep"`
+	ScoreAlignment    int       `json:"score_alignment"`
+	Total             int       `json:"total"`
+	Duration          int       `json:"duration"`
+	Awake             int       `json:"awake"`
+	Light             int       `json:"light"`
+	Rem               int       `json:"rem"`
+	Deep              int       `json:"deep"`
+	OnsetLatency      int       `json:"onset_latency"`
+	Restless          int       `json:"restless"`
+	Efficiency        int       `json:"efficiency"`
+	MidpointTime      int       `json:"midpoint_time"`
+	HrLowest          int       `json:"hr_lowest"`
+	HrAverage         float64   `json:"hr_average"`
+	Rmssd             int       `json:"rmssd"`
+	BreathAverage     float64   `json:"breath_average"`
+	TemperatureDelta  float64   `json:"temperature_delta"`
+	Hypnogram5Min     string    `json:"hypnogram_5min"`
+	Hr5Min            []int     `json:"hr_5min"`
+	Rmssd5Min         []int     `json:"rmssd_5min"`
+}
 
 func makeRequest(path string) ([]byte, error) {
 	// create request
